fa2fa48f6356efd5361987a88504bae3d73dcfb0
Michael Bäck <majkieboy@gmail.com>
2021-03-28 06:36:15 +0200

    Takes string input from user and writes to console.

--- Python/python.py
+++ Python/python.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-# use with either python3 python.py or chmod +x and ./python.py (python will refer to python 2)
+# Use with either python3 python.py or chmod +x and ./python.py (python will refer to python 2)
+# Takes string input from user and writes to console.
 
 def main():
     myText = input("Enter some text:\n")
