d87d5ae9964e5bdd6a0e78e43c1f82d95f299e85
Michael Bäck <majkieboy@gmail.com>
2021-03-30 01:18:00 +0200

    Added Perl.

---
+++ rnd_number/Perl/rnd.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+# Run with perl rndNr.pl
+# Generates and prints a random number from 0 to 1000
+
+my $rndNr = int(rand(1001)); #Higher number is exclusive just like in C#
+print $rndNr, "\n";
