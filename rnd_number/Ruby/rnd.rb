0faae82fb5a1e2c214bac85ffd579c8fedc2672f
Michael Bäck <majkieboy@gmail.com>
2021-03-31 11:18:54 +0200

    Added Ruby.

---
+++ rnd_number/Ruby/rnd.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+# Use with ruby message.rb or chmod +x and ./message.rb
+# Generates and prints a random number between 0 and 1000
+
+rndNr = rand(1001)
+
+puts "Your random number is: #{rndNr}"
