6cb4e65e4f9d1756f663aa0e54f0f3167fb12009
Michael Bäck <majkieboy@gmail.com>
2021-03-31 19:06:57 +0200

    Fixed range, previously 1-1000.

--- rnd_number/PHP/rand.php
+++ rnd_number/PHP/rand.php
@@ -1,6 +1,6 @@
 <?php
 // Use with php rand.php or in website with php module
-// Generates and prints a random number from 1 to 1000
-echo rand(1,1000), "\n";
+// Generates and prints a random number from 0 to 1000
+echo rand(0,1000), "\n";
 
 ?>
