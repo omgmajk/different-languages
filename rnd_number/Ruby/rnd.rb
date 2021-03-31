fc28033c17c94175a64ee9aeb7f503897049f341
Michael Bäck <majkieboy@gmail.com>
2021-03-31 11:20:37 +0200

    Fixed typo.

--- rnd_number/Ruby/rnd.rb
+++ rnd_number/Ruby/rnd.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/ruby
-# Use with ruby message.rb or chmod +x and ./message.rb
+# Use with ruby rnd.rb or chmod +x and ./rnd.rb
 # Generates and prints a random number between 0 and 1000
 
 rndNr = rand(1001)
