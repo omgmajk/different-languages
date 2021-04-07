c1cec595b42166d956e78d01ca2d20b5f2ee6db5
omgmajk <majkieboy@gmail.com>
2021-04-07 12:35:44 +0200

    Fixed a typo.

--- user_input/JavaScript/message.js
+++ user_input/JavaScript/message.js
@@ -1,5 +1,5 @@
 // use js message.js to run
-// Written in node
+// Written in NodeJs
 // Takes string input from user and writes to console.
 const readline = require("readline");
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
