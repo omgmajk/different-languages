4fbd02a3c62daf4073c05d94817ce7b733e44aaa
omgmajk <majkieboy@gmail.com>
2021-05-22 23:07:30 +0200

    Added some code.

--- rnd_number/Fortran/rnd.f90
+++ rnd_number/Fortran/rnd.f90
@@ -2,7 +2,11 @@
 !Prints a real number as random
 
 program RndNr
+
+implicit none
+
     real :: r(1)
+    write (*,'("Here is a real (pseudo) random number:")')
     call random_number(r)
     print *, r
 
