9a11135c46a687f3dc5e52e1d4cc3a6c69daa13b
Michael Bäck <majkieboy@gmail.com>
2021-07-18 17:44:45 -0400

    Added Rust.

---
+++ fizzbuzz/Rust/fizzbuzz.rs
@@ -0,0 +1,23 @@
+// Compile using rustc.rs
+// Classic fizzbuzz problem from 1 to 100
+
+fn main(){
+    let mut i = 1;
+    
+    while i <= 100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        }
+        else {
+            println!("{}", i);
+        }
+
+        i += 1;
+    }
+
+
+}
