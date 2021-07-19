32cac400e710d2522fc192a0833314ec3c64540b
Michael Bäck <majkieboy@gmail.com>
2021-07-18 20:01:42 -0400

    Fixed fortran code to print between 1-1000

--- rnd_number/Fortran/rnd.f90
+++ rnd_number/Fortran/rnd.f90
@@ -1,5 +1,5 @@
 !Compile with gfortran rnd.f90 and run with ./a.out
-!Prints a real number as random
+!Prints a real number as random between 1-1000
 
 program RndNr
 
@@ -8,6 +8,6 @@ implicit none
     real :: r(1)
     write (*,'("Here is a real (pseudo) random number:")')
     call random_number(r)
-    print *, r
+    print *, floor(r * 1000)
 
 end program
