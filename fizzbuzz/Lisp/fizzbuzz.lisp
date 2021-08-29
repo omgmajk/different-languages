a767fd8de446774553f1cf43b4b752eb048a88f0
Michael Bäck <majkieboy@gmail.com>
2021-08-29 10:15:44 -0400

    Added comments.

--- fizzbuzz/Lisp/fizzbuzz.lisp
+++ fizzbuzz/Lisp/fizzbuzz.lisp
@@ -3,13 +3,12 @@
 
 (loop for i from 1 to 100 do
     (
-        let (
+        let ( ;; modThree and modFive will be set to true if zerop is equal to zero
             (modThree (zerop (mod i 3)))
             (modFive  (zerop (mod i 5)))
         )
 
-        (cond
-            (
+        (cond (
                 (and modThree modFive)
                 (write-line "FizzBuzz")
             )
@@ -22,7 +21,7 @@
                 (write-line "Buzz")
             )
             (
-                t
+                t ;; Returned from zerop above when i is not modFive or modThree
                 (format t "~d~%" i)
             )
         )
