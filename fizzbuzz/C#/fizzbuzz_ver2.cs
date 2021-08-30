53d176083a8e298f7b14483a8f1e56eeeb2128bf
Michael Bäck <majkieboy@gmail.com>
2021-08-30 16:16:15 -0400

    Fixed typo.

--- fizzbuzz/C#/fizzbuzz_ver2.cs
+++ fizzbuzz/C#/fizzbuzz_ver2.cs
@@ -2,7 +2,7 @@
 using System;
 class Program {
     static void Main() {
-        for (int i = 0; i <= 100; i++)
+        for (int i = 1; i <= 100; i++)
             Console.WriteLine(i % 5 == 0 && i % 3 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i);
     }
 }
